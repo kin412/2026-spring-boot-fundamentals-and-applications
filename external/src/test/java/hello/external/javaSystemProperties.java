42ccbb3c9f7b913a7bd01efe60d03295cb2b8957
MINDEV\kin41 <kin412@naver.com>
2026-02-19 23:12:49 +0900

    외부설정과 프로필1 - 외부 설정 종류

---
+++ external/src/test/java/hello/external/javaSystemProperties.java
@@ -0,0 +1,31 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class javaSystemProperties {
+
+    public static void main(String[] args) {
+
+        System.setProperty("hello_key","hello_value");
+        String hello_Key = System.getProperty("hello_key");
+        log.info("hello_Key={}", hello_Key);
+
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            log.info("prop {}={}", key, System.getProperty(String.valueOf(key)));
+        }
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url={}", url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+
+    }
+
+}
