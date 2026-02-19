85b3460790970a76a3b370ce50a095663dca6a2d
MINDEV\kin41 <kin412@naver.com>
2026-02-20 02:29:14 +0900

    외부설정과 프로필2 - @Profile

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -13,8 +13,8 @@ import org.springframework.context.annotation.Import;
 //@Import(MyDataSourceConfigV2.class)
 @Import(MyDataSourceConfigV3.class)
 //hello.config에 설정파일을 단걔적으로 계속 추가할건데 이 설정파일들을 중복으로 적용하지 않기 위해서
-@SpringBootApplication(scanBasePackages = "hello.datasource")
-@ConfigurationPropertiesScan({"hello"}) // properties용 컴포넌트 스캔
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
+//@ConfigurationPropertiesScan({"hello"}) // properties용 컴포넌트 스캔
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
