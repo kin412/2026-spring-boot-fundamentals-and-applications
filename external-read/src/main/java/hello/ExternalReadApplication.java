154ba0be5b62405a8762a85d8dd0a58574a3999b
MINDEV\kin41 <kin412@naver.com>
2026-02-20 01:13:16 +0900

    외부설정과 프로필2 - 외부 설정 사용 - Environment

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,13 @@
 package hello;
 
+import hello.config.MyDatasourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDatasourceEnvConfig.class)
+//hello.config에 설정파일을 단걔적으로 계속 추가할건데 이 설정파일들을 중복으로 적용하지 않기 위해서
+@SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
